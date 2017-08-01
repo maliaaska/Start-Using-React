ec9ff7fb89b695f7b91cff337625ee2351d8cb26
Arkadiusz <asmilewski@gmail.com>
2017-08-01 17:57:14 +0200

    express login signup working

---
+++ GameDayApp/express-GameDay/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const passport   = require('./config/passport');
+const cors = require('cors')({ exposedHeaders: ['X-ResponseTime'] });
+
+
+const auth = require('./routes/auth-routes');
+
+require("dotenv").config();
+require('./config/database');
+
+const app = express();
+
+// view engine setup
+
+app.use(cors);
+app.options('*', cors);
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', index);
+app.use('/', auth);
+app.use('/api', passport.authenticate('jwt', { session: false }));
+// app.use('/api', phones);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+
+});
+
+module.exports = app;
