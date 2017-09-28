571186e310543e2eaef0fdbc5259f50c5785da3f
Arkadiusz <asmilewski@gmail.com>
2017-09-28 18:55:23 +0200

    widgets working

--- src/App.js
+++ src/App.js
@@ -15,13 +15,22 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" onChange={this.update.bind(this)}/>
-
-        <h1>{this.state.txt} - {this.state.cat}</h1>
+        <h1>{this.state.txt}</h1>
+       <Widget update={this.update.bind(this)} />
+       <Widget update={this.update.bind(this)} />
+       <Widget update={this.update.bind(this)} />
+       <Widget update={this.update.bind(this)} />
+       <Widget update={this.update.bind(this)} />
+       
       </div>
     )
   }
 }
+
+const Widget =  (props) => 
+  <input type="text"
+  onChange={props.update}/>
+
 export default App
 
 //     let txt = this.props.txt     let cat = this.props.cat;     return ( <div>
