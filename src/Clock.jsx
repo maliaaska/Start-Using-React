0fce73a9a8fa8ce63e273f43a7dd00bfaf66dfd2
Arkadiusz <asmilewski@gmail.com>
2017-09-30 17:29:02 +0200

    clock is counting down in real time

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -13,6 +13,14 @@ class Clock extends Component {
         }
         console.log('this.props', this.props)
     }
+
+    componentWillMount() {
+        this.getTimeUntil(this.props.deadline);
+    }
+
+    componentDidMount(){
+        setInterval(() => this.getTimeUntil(this.props.deadline))
+    }
         getTimeUntil(deadline){
             const time = Date.parse(deadline) - Date.parse(new Date())
             console.log('time', time);
@@ -22,11 +30,11 @@ class Clock extends Component {
             const days = Math.floor(time/(1000*60*60*24));
 
             console.log('seconds', seconds, "minutes", minutes, 'hours', hours, 'days', days)
-
-        
+            this.setState({days, hours, minutes, seconds});
+            
+            
     }
     render (){
-        this.getTimeUntil(this.props.deadline);
 
         return(<div>
             <div className="Clock-days">{this.state.days} Days</div>
